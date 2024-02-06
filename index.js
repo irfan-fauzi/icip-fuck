63e1502b5c5f70ad4775253a99ea19ec1324a930
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-07 00:26:28 +0700

    test send data

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 9000
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-  res.send("hallo server sedang berjalan yeyeyey")
-})
+app.get("/", (req, res) => {
+  res.status(200).send({
+    data: [
+      {
+        id: 1,
+        name: "irfan",
+      },
+      {
+        id: 2,
+        name: "jaka",
+      },
+    ],
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`server SEDANG BERJALAN DI port: ${PORT}`)
-})
+  console.log(`server SEDANG BERJALAN DI port: ${PORT}`);
+});
