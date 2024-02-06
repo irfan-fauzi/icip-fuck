b30e902d5b480447594fdf3d4adae77bc73f17aa
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-06 22:52:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 9000
+
+app.get('/', (req, res) => {
+  res.send("hallo api is fucking work")
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`)
+})
