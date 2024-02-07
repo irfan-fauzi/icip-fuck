6a13aa083ec122bbc8bcb34a95981ea13001f217
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-07 22:20:39 +0700

    remove env

--- config/Database.js
+++ config/Database.js
@@ -1,23 +1,23 @@
-// import { Sequelize } from "sequelize";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-// const db = new Sequelize(
-//   process.env.DATABASE_NAME,
-//   process.env.DATABASE_USERNAME,
-//   process.env.DATABASE_PASSWORD,
-//   {
-//     host: "sql6.freesqldatabase.com",
-//     dialect: "mysql",
-//   }
-// );
-
-// export default db;
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("sql6681491", "sql6681491", "HcnF6hsPAq", {
-  host: "sql6.freesqldatabase.com",
-  dialect: "mysql",
-});
+dotenv.config();
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER_NAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: "sql6.freesqldatabase.com",
+    dialect: "mysql",
+  }
+);
 
 export default db;
+// import { Sequelize } from "sequelize";
+
+// const db = new Sequelize("sql6681491", "sql6681491", "HcnF6hsPAq", {
+//   host: "sql6.freesqldatabase.com",
+//   dialect: "mysql",
+// });
+
+// export default db;
