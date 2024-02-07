7211b7a3991e18f641d161d9ad2a2f0acce76164
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-07 22:33:37 +0700

    change hosting db

--- config/Database.js
+++ config/Database.js
@@ -7,17 +7,10 @@ const db = new Sequelize(
   process.env.DATABASE_USER_NAME,
   process.env.DATABASE_PASSWORD,
   {
-    host: "sql6.freesqldatabase.com",
+    host: process.env.DATABASE_HOSTING,
     dialect: "mysql",
   }
 );
 
 export default db;
-// import { Sequelize } from "sequelize";
 
-// const db = new Sequelize("sql6681491", "sql6681491", "HcnF6hsPAq", {
-//   host: "sql6.freesqldatabase.com",
-//   dialect: "mysql",
-// });
-
-// export default db;
